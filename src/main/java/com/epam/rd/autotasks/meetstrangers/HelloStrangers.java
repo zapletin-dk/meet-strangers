b71f3707a824193b1047b0646a163b1e0aa43643
Zapletin Daniil <Zaplot1994@gmail.com>
2023-03-06 20:36:26 +0300

    renaming variable "counter" to "counterOfPeople"

--- src/main/java/com/epam/rd/autotasks/meetstrangers/HelloStrangers.java
+++ src/main/java/com/epam/rd/autotasks/meetstrangers/HelloStrangers.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 public class HelloStrangers {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int counter = Integer.parseInt(scan.nextLine());
-        if (counter == 0){
+        int counterOfPeople = Integer.parseInt(scan.nextLine());
+        if (counterOfPeople == 0){
             System.out.println("Oh, it looks like there is no one here");
-        } else if (counter<0) {
+        } else if (counterOfPeople < 0) {
             System.out.println("Seriously? Why so negative?");
         }else {
-            for (int i = 0; i < counter; i++) {
+            for (int i = 0; i < counterOfPeople; i++) {
                 System.out.printf("Hello, %s\n", scan.nextLine());
             }
         }
